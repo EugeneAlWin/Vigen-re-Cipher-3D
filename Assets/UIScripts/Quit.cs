d9d3ced6677d0b97c5b24ee3168715e336132aa8
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-02-22 21:18:06 +0300

    models

--- Assets/UIScripts/Quit.cs
+++ Assets/UIScripts/Quit.cs
@@ -3,15 +3,8 @@ using UnityEngine.EventSystems;
 
 public class Quit : MonoBehaviour, IPointerClickHandler
 {
-    Matrix gm;
-    void Start()
-    {
-        gm = GameObject.Find("Container").GetComponent<Matrix>();
-
-    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        gm.DestroyMatrix();
         Application.Quit();
     }
 }
