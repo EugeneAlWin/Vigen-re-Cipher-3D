a40d847d18d3fb283a29eebb65dbb8005f9affe6
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-03-14 19:31:03 +0300

    Element Transform Update

--- Assets/Scripts/Matrixes/ElementTransform.cs
+++ Assets/Scripts/Matrixes/ElementTransform.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 class ElementTransform
 {
+    public Vector3 Position { get; set; }
     public Quaternion Rotation { get; set; }
     public Vector3 Scale { get; set; }
-    public ElementTransform(Vector3 rotation, Vector3 scale)
+    public ElementTransform(Vector3 position, Vector3 rotation, Vector3 scale)
     {
+        Position = position;
         Rotation = Quaternion.Euler(rotation);
         Scale = scale;
     }
