d6a90ab3a62df9c00811164938fb911f507205d2
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-03-19 21:03:34 +0300

    Controller class draft a few enums, delegate, Method to set current step

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
+public delegate void StepsDelegate(Controller.Steps newStep);
 public class Controller : MonoBehaviour
 {
+    public enum Steps
+    {
+        None,
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth
+    }
+    public enum Actions
+    {
+        Encoding,
+        Decoding
+    }
+    public static Steps CurrentStep { get; set; } = Steps.None;
 
+    public static Actions CurrentAction = Actions.Encoding;
+    public static StepsDelegate stepsDelegate;
+
+    private void Awake()
+    {
+        stepsDelegate += SetCurrentStep;
+    }
+
+    void SetCurrentStep(Steps newStep)
+    {
+        CurrentStep = newStep;
+    }
 }
