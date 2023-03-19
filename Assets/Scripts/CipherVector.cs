67302236554642063685f3d39ccc6e028710a3eb
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-03-19 23:32:52 +0300

    Cipher Vector added

---
+++ Assets/Scripts/CipherVector.cs
@@ -0,0 +1,9 @@
+public class CipherVector
+{
+    public string Message { get; set; }
+    public string Key { get; set; }
+    public int Depth { get; set; }
+    public char Direction { get; set; }
+    public int Step { get; set; }
+    public string AlphabetType { get; set; }
+}
