99cc2f002d49e838a60121cb51693577bc8578fd
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-04-17 00:25:19 +0300

    regex fix

--- Assets/Scripts/UIScripts/InputRegex/InputRegex.cs
+++ Assets/Scripts/UIScripts/InputRegex/InputRegex.cs
@@ -3,10 +3,11 @@ using System.Text.RegularExpressions;
 internal static class InputRegex
 {
     internal static readonly Regex latMessage = new(@"^[a-z]*$");
-    internal static readonly Regex cyrMessage = new(@"^[à-ÿ]*$");
+    internal static readonly Regex cyrMessage = new(@"^([à-ÿ]|¸)*$");
     internal static readonly Regex latKey = new(@"^[a-z]*$");
-    internal static readonly Regex cyrKey = new(@"^[à-ÿ]*$");
+    internal static readonly Regex cyrKey = new(@"^([à-ÿ]|¸)*$");
     internal static readonly Regex Depth = new(@"^[0-9]*$");
     internal static readonly Regex Step = new(@"^[0-9]*$");
+
     internal static bool IsMatch(string text, Regex regex) => regex.IsMatch(text);
 }
